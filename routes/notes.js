65bd8d090d92d6fc75d13332712b645c3a790355
Nathan Hoang <sliqq123@gmail.com>
2023-10-07 16:50:34 -0700

    added get route in notes.js wtih readFromFile function

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const notes = require("express").Router();
+const dbPath = path.join(__dirname, '../db/db.json'); // db path
 
+const readFromFile = () => {
+  return JSON.parse(fs.readFileSync(dbPath, 'utf8')); //function to read file
+};
 
+notes.get('/', (req, res) => { // get route reads from db and sends json response of db object
+    res.json(readFromFile());
+});
 module.exports = notes;
